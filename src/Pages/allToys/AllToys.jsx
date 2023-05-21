d1795201e8842fd0dec8be00ab292e616dd909a5
Mubasshirah <adibaju1994@gmail.com>
2023-05-21 15:32:23 +0600

    all toys page done

--- src/Pages/allToys/AllToys.jsx
+++ src/Pages/allToys/AllToys.jsx
@@ -1,9 +1,45 @@
+import { useLoaderData } from "react-router-dom";
+import SingleToyCard from "./SingleToyCard";
 
 
 const AllToys = () => {
+    const toys=useLoaderData()
     return (
-        <div>
-            this is alltoy
+        
+               <div>
+            <h2 className="text-5xl text-sky-500 my-5 text-center">Grab Your favourite toys!</h2>
+            <div className="overflow-x-auto ">
+                <table className="table table-compact w-full">
+                    {/* head */}
+                    <thead>
+                        <tr>
+                            
+                            <th>
+                                <p>Picture and</p>
+                                <p>Toy Name</p>
+                            </th>
+                            <th>Description</th>
+                            <th>Sub Category</th>
+                            <th>Seller</th>
+                            <th> 
+                                <p>Price and</p>
+                                <p>available quantity</p>
+                            </th>
+                            <th>
+                                <p>Status</p>
+                            </th>
+                        </tr>
+                    </thead>
+                    <tbody >
+                          {toys.map(toy=><SingleToyCard toy={toy} key={toy._id}></SingleToyCard>)}
+                    </tbody>
+
+                </table>
+            </div>
+        
+
+
+          
         </div>
     );
 };
---
+++ src/Pages/allToys/SingleToyCard.jsx
@@ -0,0 +1,41 @@
+
+
+const SingleToyCard = ({ toy }) => {
+    const { _id, name, seller_name, sub_category, price, available_quantity, picture, rating, description } = toy;
+    return (
+    
+            <tr>
+            <th >
+                <div>
+                    <div className="rounded w-24 h-24">
+                        <img src={picture} alt="image" />
+                    </div>
+                    <p>{name}</p>
+                </div>
+            </th>
+            <td >
+            <p className="w-[15%]">
+                {description}
+              </p>
+            </td>
+            <td >
+              <p>{sub_category}</p>
+              
+            </td>
+            <td >
+            <p>{seller_name}</p>
+            </td>
+            <td >
+            <p>${price}</p>
+            <p>{available_quantity}</p>
+            </td>
+               
+               <td >
+                <button className="btn btn-info">View Details</button>
+               </td>
+        </tr>
+        
+    );
+};
+
+export default SingleToyCard;
