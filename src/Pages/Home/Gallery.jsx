e5fd795da82740176876f44261b4082ae9f070c9
Mubasshirah <adibaju1994@gmail.com>
2023-05-23 23:37:20 +0600

    done

--- src/Pages/Home/Gallery.jsx
+++ src/Pages/Home/Gallery.jsx
@@ -4,45 +4,47 @@ import img3 from '../../assets/images/img3.jpg';
 import img4 from '../../assets/images/img4.jpg';
 import img5 from '../../assets/images/img5.jpg';
 import img6 from '../../assets/images/img6.jpg';
-
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+import { useEffect } from 'react';
 const Gallery = () => {
+  useEffect(()=>{
+    AOS.init({duration:3000})
+},[])
     return (
         <div className='mb-10'>
              <h1 className='text-5xl my-10 text-center '> Our Gallery</h1>
 
-             <div className='grid grid-cols-1 md:grid-cols-3 gap-4'>
-              <div className='h-80 border-8 border-sky-500'>
-                <img src={img1} alt="" className=' h-full '/>
-              </div>
-
+             <div className='grid grid-cols-1 md:grid-cols-2 gap-4'>
+             
 
               <div className='grid md:grid-rows-2 gap-6'>
-                <div className='h-80 md:h-36  border-8 border-red-400'><img src={img2} alt="" className='h-full w-full' /></div>
-                <div className='h-80 md:h-36 border-8 border-amber-400'><img src={img5} alt="" className='h-full w-full'/></div>
+                <div className='h-80 md:h-36  '><img src={img2} alt="" className='h-full shadow-lg w-full' /></div>
+                <div className='h-80 md:h-36 '><img src={img5} alt="" className='h-full w-full shadow-lg'/></div>
               </div>
 
 
-              <div className='h-80 border-8 border-red-400'>
-                <img src={img3} alt="" className=' h-full '/>
+              <div className='h-80 '>
+                <img src={img3} alt="" className=' h-full shadow-lg'/>
               </div>
              </div>
 
 
              <div className='grid grid-cols-1 md:grid-cols-3 gap-4 mt-5'>
-              <div className='h-80 w-full border-8 border-sky-500'>
-                <img src={img6} alt="" className=' h-full w-full'/>
+              <div className='h-80 w-full '>
+                <img src={img6} alt="" className=' h-full w-full shadow-lg'/>
               </div>
 
 
               
 
-              <div className='h-80 w-full border-8 border-lime-500'>
-                <img src={img5} alt="" className=' h-full w-full'/>
+              <div className='h-80 w-full '>
+                <img src={img5} alt="" className=' h-full w-full shadow-lg'/>
               </div>
 
               <div className='grid md:grid-rows-2 gap-6'>
-                <div className='h-80 md:h-36  border-8 border-sky-500'><img src={img4} alt="" className='h-full w-full' /></div>
-                <div className='h-80 md:h-36 border-8 border-lime-500'><img src={img1} alt="" className='h-full w-full'/></div>
+                <div className='h-80 md:h-36  '><img src={img4} alt="" className='h-full w-full shadow-lg' /></div>
+                <div className='h-80 md:h-36 '><img src={img1} alt="" className='h-full w-full shadow-lg'/></div>
               </div>
 
              </div>
--- src/Pages/Home/Home/CustomersReview.jsx
+++ src/Pages/Home/Home/CustomersReview.jsx
@@ -1,11 +1,17 @@
 
-import customer from '../../../assets/images/ccustomer.png'
+import customer from '../../../assets/images/ccustomer.png';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+import { useEffect } from 'react';
 const CustomersReview = () => {
+    useEffect(()=>{
+        AOS.init({duration:3000})
+    },[])
     return (
         <div>
             <h1 className='text-5xl my-10 text-center '> Customers Review</h1>
 
-            <div className="card lg:card-side bg-base-200 shadow-xl md:w-2/3 mx-auto mb-10">
+            <div className="card lg:card-side bg-base-200 shadow-xl md:w-2/3 mx-auto mb-10" >
                 <figure><img src={customer} /></figure>
                 <div className="card-body">
                     <h2 className="card-title">Ms.Azmin</h2>
@@ -14,16 +20,17 @@ const CustomersReview = () => {
 
                 </div>
             </div>
-            <div className="card lg:card-side bg-base-200 shadow-xl md:w-2/3 mx-auto mb-10">
-                <figure><img src={customer} /></figure>
+            <div className="card lg:card-side bg-base-200 shadow-xl md:w-2/3 mx-auto mb-10" data-aos='fade-right'>
+                
                 <div className="card-body">
                     <h2 className="card-title">Ms.Sara</h2>
                     <p>I am highly pleased with this toys. These are really educative and funny.My children are happy as well. I will buy more toys fromthis store further</p>
 
 
                 </div>
+                <figure><img src={customer} /></figure>
             </div>
-            <div className="card lg:card-side bg-base-200 shadow-xl md:w-2/3 mx-auto mb-10">
+            <div className="card lg:card-side bg-base-200 shadow-xl md:w-2/3 mx-auto mb-10" data-aos='fade-left'>
                 <figure><img src={customer} /></figure>
                 <div className="card-body">
                     <h2 className="card-title">Ms.Amrin</h2>
@@ -32,16 +39,17 @@ const CustomersReview = () => {
 
                 </div>
             </div>
-            <div className="card lg:card-side bg-base-200 shadow-xl md:w-2/3 mx-auto mb-10">
-                <figure><img src={customer} /></figure>
+            <div className="card lg:card-side bg-base-200 shadow-xl md:w-2/3 mx-auto mb-10" data-aos='fade-right'>
+                
                 <div className="card-body">
                     <h2 className="card-title">Ms.Anika</h2>
                     <p>I am highly pleased with this toys. These are really educative and funny.My children are happy as well. I will buy more toys fromthis store further</p>
 
 
                 </div>
+                <figure><img src={customer} /></figure>
             </div>
-            <div className="card lg:card-side bg-base-200 shadow-xl md:w-2/3 mx-auto mb-10">
+            <div className="card lg:card-side bg-base-200 shadow-xl md:w-2/3 mx-auto mb-10" data-aos='fade-left'>
                 <figure><img src={customer} /></figure>
                 <div className="card-body">
                     <h2 className="card-title">Ms.Nazifa</h2>
@@ -50,14 +58,15 @@ const CustomersReview = () => {
 
                 </div>
             </div>
-            <div className="card lg:card-side bg-base-200 shadow-xl md:w-2/3 mx-auto mb-10">
-                <figure><img src={customer} /></figure>
+            <div className="card lg:card-side bg-base-200 shadow-xl md:w-2/3 mx-auto mb-10" data-aos='fade-right'>
+                
                 <div className="card-body">
                     <h2 className="card-title">Ms.Samia</h2>
                     <p>I am highly pleased with this toys. These are really educative and funny.My children are happy as well. I will buy more toys fromthis store further</p>
 
 
                 </div>
+                <figure><img src={customer} /></figure>
             </div>
         </div>
 
